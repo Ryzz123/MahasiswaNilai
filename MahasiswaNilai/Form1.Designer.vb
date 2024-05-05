17ce9eaf3d4d138e58c27859cea26027fa9d2ecf
Ryzz <febryananda17@gmail.com>
2024-05-05 14:46:03 +0700

    Add project files.

---
+++ MahasiswaNilai/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        Label4 = New Label()
+        TextBox4 = New TextBox()
+        Label5 = New Label()
+        TextBox5 = New TextBox()
+        Label6 = New Label()
+        TextBox6 = New TextBox()
+        Label7 = New Label()
+        TextBox7 = New TextBox()
+        Label8 = New Label()
+        TextBox8 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Microsoft Sans Serif", 8.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(309, 70)
+        TextBox1.Name = "TextBox1"
+        TextBox1.PlaceholderText = "Masukan nama "
+        TextBox1.Size = New Size(151, 21)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = SystemColors.ControlText
+        Label1.Location = New Point(309, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(43, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "NAMA"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = SystemColors.ControlText
+        Label2.Location = New Point(476, 52)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(34, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "NPM"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(476, 69)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PlaceholderText = "Masukan NPM"
+        TextBox2.Size = New Size(151, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.ForeColor = SystemColors.ControlText
+        Label3.Location = New Point(311, 109)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "PRODI"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(309, 127)
+        TextBox3.Name = "TextBox3"
+        TextBox3.PlaceholderText = "Masukan PRODI"
+        TextBox3.Size = New Size(151, 23)
+        TextBox3.TabIndex = 4
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = SystemColors.ControlText
+        Label4.Location = New Point(476, 109)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(59, 15)
+        Label4.TabIndex = 7
+        Label4.Text = "FAKULTAS"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(476, 127)
+        TextBox4.Name = "TextBox4"
+        TextBox4.PlaceholderText = "Masukan FAKULTAS"
+        TextBox4.Size = New Size(151, 23)
+        TextBox4.TabIndex = 6
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.ForeColor = SystemColors.ControlText
+        Label5.Location = New Point(311, 186)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(51, 15)
+        Label5.TabIndex = 9
+        Label5.Text = "HARIAN"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Font = New Font("Microsoft Sans Serif", 8.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox5.Location = New Point(311, 204)
+        TextBox5.Name = "TextBox5"
+        TextBox5.PlaceholderText = "Masukan Nilai HARIAN "
+        TextBox5.Size = New Size(151, 21)
+        TextBox5.TabIndex = 8
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.ForeColor = SystemColors.ControlText
+        Label6.Location = New Point(476, 186)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(43, 15)
+        Label6.TabIndex = 11
+        Label6.Text = "ABSEN"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Font = New Font("Microsoft Sans Serif", 8.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox6.Location = New Point(476, 204)
+        TextBox6.Name = "TextBox6"
+        TextBox6.PlaceholderText = "Masukan Nilai ABSEN "
+        TextBox6.Size = New Size(151, 21)
+        TextBox6.TabIndex = 10
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.ForeColor = SystemColors.ControlText
+        Label7.Location = New Point(476, 249)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(29, 15)
+        Label7.TabIndex = 15
+        Label7.Text = "UAS"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Font = New Font("Microsoft Sans Serif", 8.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox7.Location = New Point(476, 267)
+        TextBox7.Name = "TextBox7"
+        TextBox7.PlaceholderText = "Masukan Nilai UAS "
+        TextBox7.Size = New Size(151, 21)
+        TextBox7.TabIndex = 14
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.ForeColor = SystemColors.ControlText
+        Label8.Location = New Point(311, 249)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(27, 15)
+        Label8.TabIndex = 13
+        Label8.Text = "UTS"
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Font = New Font("Microsoft Sans Serif", 8.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox8.Location = New Point(311, 267)
+        TextBox8.Name = "TextBox8"
+        TextBox8.PlaceholderText = "Masukan Nilai UTS "
+        TextBox8.Size = New Size(151, 21)
+        TextBox8.TabIndex = 12
+        ' 
+        ' Button1
+        ' 
+        Button1.ForeColor = SystemColors.ActiveCaptionText
+        Button1.Location = New Point(526, 324)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(103, 24)
+        Button1.TabIndex = 16
+        Button1.Text = "KONFIRMASI"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(971, 508)
+        Controls.Add(Button1)
+        Controls.Add(Label7)
+        Controls.Add(TextBox7)
+        Controls.Add(Label8)
+        Controls.Add(TextBox8)
+        Controls.Add(Label6)
+        Controls.Add(TextBox6)
+        Controls.Add(Label5)
+        Controls.Add(TextBox5)
+        Controls.Add(Label4)
+        Controls.Add(TextBox4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        ForeColor = SystemColors.ControlLight
+        Name = "Form1"
+        Text = "Nilai Mahasiswa"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ MahasiswaNilai/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private Function Validasi() As Boolean
+        If TextBox1.Text = "" Then
+            MessageBox.Show("Nama Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox1.Focus()
+            Return False
+        ElseIf TextBox2.Text = "" Then
+            MessageBox.Show("NIM Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox2.Focus()
+            Return False
+        ElseIf TextBox3.Text = "" Then
+            MessageBox.Show("Prodi Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox3.Focus()
+            Return False
+        ElseIf TextBox4.Text = "" Then
+            MessageBox.Show("Fakultas Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox4.Focus()
+            Return False
+        ElseIf TextBox5.Text = "" Then
+            MessageBox.Show("Nilai Harian Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox5.Focus()
+            Return False
+        ElseIf TextBox6.Text = "" Then
+            MessageBox.Show("Nilai Absen Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox6.Focus()
+            Return False
+        ElseIf TextBox7.Text = "" Then
+            MessageBox.Show("Nilai UAS Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox7.Focus()
+            Return False
+        ElseIf TextBox8.Text = "" Then
+            MessageBox.Show("Nilai UTS Harus Diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox8.Focus()
+            Return False
+        End If
+        Return True
+    End Function
+    
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not Validasi() Then
+            Return
+        End If
+        
+        Dim Nama As String = TextBox1.Text
+        Dim NIM As String = TextBox2.Text
+        Dim Prodi As String = TextBox3.Text
+        Dim Fakultas As String = TextBox4.Text
+
+        Dim NilaiHarian As Single = Val(TextBox5.Text)*0.2
+        Dim NilaiAbsen As Single = Val(TextBox6.Text)*0.2
+        Dim NilaiUTS As Single = Val(TextBox8.Text)*0.25
+        Dim NilaiUAS As Single = Val(TextBox7.Text)*0.25
+
+        Dim NilaiAkhir As Integer = NilaiHarian + NilaiAbsen + NilaiUTS + NilaiUAS
+
+        If NilaiAkhir >= 80 Then
+            MessageBox.Show(
+                "Selamat " & Nama & " dengan NIM " & NIM & " dari Prodi " & Prodi & " Fakultas " & Fakultas &
+                " Anda Lulus dengan Nilai " & NilaiAkhir, "A", MessageBoxButtons.OK,
+                MessageBoxIcon.Information)
+        ElseIf NilaiAkhir >= 70 Then
+            MessageBox.Show(
+                "Selamat " & Nama & " dengan NIM " & NIM & " dari Prodi " & Prodi & " Fakultas " & Fakultas &
+                " Anda Lulus dengan Nilai " & NilaiAkhir, "B", MessageBoxButtons.OK,
+                MessageBoxIcon.Information)
+        ElseIf NilaiAkhir >= 60 Then
+            MessageBox.Show(
+                "Selamat " & Nama & " dengan NIM " & NIM & " dari Prodi " & Prodi & " Fakultas " & Fakultas &
+                " Anda Lulus dengan Nilai " & NilaiAkhir, "C", MessageBoxButtons.OK,
+                MessageBoxIcon.Information)
+        ElseIf NilaiAkhir >= 50 Then
+            MessageBox.Show(
+                "Selamat " & Nama & " dengan NIM " & NIM & " dari Prodi " & Prodi & " Fakultas " & Fakultas &
+                " Anda Lulus dengan Nilai " & NilaiAkhir, "D", MessageBoxButtons.OK,
+                MessageBoxIcon.Information)
+        Else
+            MessageBox.Show(
+                "Maaf " & Nama & " dengan NIM " & NIM & " dari Prodi " & Prodi & " Fakultas " & Fakultas &
+                " Anda Tidak Lulus dengan Nilai " & NilaiAkhir, "E", MessageBoxButtons.OK,
+                MessageBoxIcon.Information)
+        End If
+    End Sub
+End Class
